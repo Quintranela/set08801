dd67dd9d35fadcbc50ef6c511b39d51f19df0bd1
Quintranela <quintra.nela@gmail.com>
2023-04-28 19:50:42 -0400

    Final submission

---
+++ Assessment_2/js/javascript.js
@@ -0,0 +1,9 @@
+
+	function increaseFont() {
+
+	  document.getElementsByTagName('body')[0].style.fontSize = "30px";
+	}
+
+	function decreaseFont() {
+	 document.getElementsByTagName('body')[0].style.fontSize = "20px";
+	}
